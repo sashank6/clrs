aa88084cee0667d4013e1e79ca9a366c30e15c64
sashank6 <sashank.alladi@live.com>
2016-12-20 07:17:42 -0800

    updated

--- chapter2/insertion_sort.cpp
+++ chapter2/insertion_sort.cpp
@@ -40,6 +40,8 @@ void insertion_sort(int arr[], int n) {
 		for (int j = i - 1; j >= 0; j--) {
 			if (arr[j] > arr[j + 1])
 				swap(arr[j], arr[j + 1]);
+			else
+				break;
 		}
 	}
 	display(arr, n);
